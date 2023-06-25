9d0b4a554fd4647d26996a11829bd181518e2387
ArinaBalakina <Balakinaarina@yandex.ru>
2023-06-25 20:57:56 +0500

    unworking-sorted

--- static/modules/filter/script.js
+++ static/modules/filter/script.js
@@ -1,6 +1,25 @@
 
 function SetButtonsActivity() {
 
+
+    let optionMenu = document.querySelector(".select-menu"),
+        selectBtn_sorted = document.querySelector(".select-btn"),
+        options = document.querySelectorAll(".option"),
+        sBtn_text = document.querySelector(".sBtn-text");
+
+    selectBtn_sorted.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+    options.forEach(option => {
+        option.addEventListener("click", () => {
+            let selectedOption = option.querySelector(".option-text").innerText;
+            sBtn_text.innerText = selectedOption;
+
+            optionMenu.classList.remove("active")
+        })
+    })
+
+
+
     let selectBtn = document.getElementById("category-btn"),
         items = document.querySelectorAll(".item");
 
